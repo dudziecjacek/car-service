1ef9d6e187a22e90d7566cee0867c5ac2e1898cb
Jack Dud <dudziecjacek@gmail.com>
2019-10-07 13:47:26 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/car-service/precache-manifest.e4533dc38ec489fb100a0ce68e0dba69.js"
+  "/car-service/precache-manifest.34807f07b8a56c8b2feddb40247ff628.js"
 );
 
 self.addEventListener('message', (event) => {
