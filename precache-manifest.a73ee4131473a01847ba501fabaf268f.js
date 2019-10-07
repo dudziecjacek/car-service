0c7f1c7b8d7a45d57190dd76aab5ec71985b32d9
Jack Dud <dudziecjacek@gmail.com>
2019-10-07 12:14:10 +0200

    Updates

---
+++ precache-manifest.a73ee4131473a01847ba501fabaf268f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35fe1959342cefef7f64975e4b1a07b3",
+    "url": "/dudziecjacek/car-service/index.html"
+  },
+  {
+    "revision": "c73ba8e92e2a64d20e2f",
+    "url": "/dudziecjacek/car-service/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "38fbec24cdfc7d7c160c",
+    "url": "/dudziecjacek/car-service/static/js/2.a57f8b23.chunk.js"
+  },
+  {
+    "revision": "c73ba8e92e2a64d20e2f",
+    "url": "/dudziecjacek/car-service/static/js/main.2956cd52.chunk.js"
+  },
+  {
+    "revision": "f990420edb1419bec0ee",
+    "url": "/dudziecjacek/car-service/static/js/runtime-main.7c35154f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/dudziecjacek/car-service/static/media/logo.25bf045c.svg"
+  }
+]);
