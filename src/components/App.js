b0bbff1c287c35ebdcd7de0e85d1030e965a5866
Jack Dud <dudziecjacek@gmail.com>
2019-10-07 13:45:44 +0200

    add Header Component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Header from './Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header location="Pęcz 15, Strzelin" hours="Poniedziałek-Piątek 08:30 - 17:30" imgUrl="../img/logo.jpg" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Header.css';
+import logo from '../img/logo.jpg';
+import 'font-awesome/css/font-awesome.min.css';
+
+class Header extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <section className="contact">
+                <div className="contact__div location contact__div--left">
+                    <div className="contact__info">
+                        <h2 className="contact__header">Lokalizacja</h2>
+                        <p>{this.props.location}</p>
+                    </div>
+                    <div className="contact__icon">
+                        <i className="fa fa-map-marker contact__clock"></i>
+                    </div>
+                </div>
+                <div className="contact__div contact__logo">
+                    <img src={logo} alt="logo" />
+                </div>
+                <div className="contact__div hours contact__div--right">
+                    <div className="contact__icon">
+                        <i className="fa fa-clock-o contact__clock" aria-hidden="true"></i>
+                    </div>
+                    <div className="contact__info">
+                        <h2 className="contact__header">Godziny otwarcia</h2>
+                        <p>{this.props.hours}</p>
+                    </div>
+                </div>
+            </section>
+        )
+    }
+}
+
+export default Header
